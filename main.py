6aadf9f688cf8eba2d134007907f5dc59cdb5f64
andrKonan <52403327+andrKonan@users.noreply.github.com>
2020-08-28 17:29:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,46 @@
-import sys, io
+import sys
 
 import qrcode
 
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit
-from PyQt5.QtCore import Qt, QSize, QRect
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QLineEdit
 
-from PIL import Image
-
-class MainWindow(QWidget):
+class MainWindow(QtWidgets.QWidget):
     def __init__(self, pathToImage = "qr.png"):
         super().__init__()
 
         self.setWindowTitle('QR code generator')
 
-        self.resize(320, 240)
-        self.setMinimumSize(QSize(320, 240))
+        self.resize(320, 320)
+        self.setMinimumSize(QtCore.QSize(320, 320))
 
         self.pathToImage = pathToImage
         self.initUI()
 
     def initUI(self):
-        grid = QGridLayout(self)
+        grid = QtWidgets.QGridLayout(self)
         grid.setSpacing(5)
 
-        qrTextInput = QLineEdit()
+        qrTextInput = QtWidgets.QLineEdit()
         qrTextInput.textChanged.connect(self.generateQR)
-        qrImage = QLabel()
-        qrImage.setPixmap(QPixmap(self.pathToImage))
+        qrImage = QtWidgets.QLabel()
         qrImage.setScaledContents(True)
 
         grid.addWidget(qrTextInput, 0, 0)
         grid.addWidget(qrImage, 1, 0)
 
         self.setLayout(grid)
-
         self.show()
+
+        self.generateQR("")
 
     def generateQR(self, text):
         qrcode.make(text).save(self.pathToImage)
 
-        label = self.findChildren(QLabel)[0]
-        label.setPixmap(QPixmap(self.pathToImage))
+        label = self.findChildren(QtWidgets.QLabel)[0]
+        label.setPixmap(QtGui.QPixmap(self.pathToImage))
 
-app = QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 
 window = MainWindow()
 
